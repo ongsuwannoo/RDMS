479f4f154ef97442f6932ce5e5536d90475cca97
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-04-15 17:30:08 +0700

    Update sidebar

--- static/js/my_js.js
+++ static/js/my_js.js
@@ -17,16 +17,20 @@ function out_hover_side_bar(event) {
 function hide_sidebar(event) {
     document.getElementById("side_bar").style.marginLeft = "-19%";
     // document.getElementById("side_bar").style.display = "none";
-    document.getElementById("hamburger").style.display = "block";
+    document.getElementById("hamburger2").style.display = "none";
+    document.getElementById("hamburger1").style.display = "block";
+    document.getElementById("hamburger1").style.zIndex = "1";
+    document.getElementById("hamburger1").style.position = "inherit";
 }
 
 function show_sidebar(event) {
     document.getElementById("side_bar").style.marginLeft = "0";
-    document.getElementById("hamburger").style.display = "none";
+    document.getElementById("hamburger1").style.display = "none";
+    document.getElementById("hamburger2").style.display = "block";
 }
 
 window.onload = function() {
     document.getElementById("hover_sidebar").addEventListener("click", click_side_bar);
     document.getElementById("hide_sidebar_btn").addEventListener("click", hide_sidebar);
-    document.getElementById("hamburger").addEventListener("click", show_sidebar);
+    document.getElementById("hamburger1").addEventListener("click", show_sidebar);
 }
