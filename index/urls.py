b25bdb099697649bc0c1b24702b493fd19de12e1
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-04-18 17:52:52 +0700

    update

--- index/urls.py
+++ index/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.my_login, name='login'),
     path('logout/', views.my_logout, name='logout')
