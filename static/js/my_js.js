f80d441b0895038a0f2f754c98a4cad89897e717
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-04-20 04:34:53 +0700

    update form

--- static/js/my_js.js
+++ static/js/my_js.js
@@ -1,13 +1,35 @@
 
 var pages = {
-    '/camp/': 'overview',
-    '/camp/campers/': 'campers',
-    '/camp/flow/': 'flow',
+    '/campers/': 'campers',
+    '/flow/': 'flow',
     '/locations/': 'locations',
-    '/camp/staffs/': 'staffs'
+    '/staffs/': 'staffs',
+    '/camp/': 'camp',
+    'camp': 'overview'
+}
+
+
+
+function hamburger(x) {
+    if (x.matches)
+        document.getElementById('menu-toggle2').style.display = 'block';
+    else
+        document.getElementById('menu-toggle2').style.display = 'none';
 }
 
 window.onload = function () {
-    document.getElementById(pages[window.location.pathname]).classList.add("active")
+    for (let key in pages) {
+        if (window.location.pathname.includes(key)){
+            if (isNaN(parseInt(window.location.pathname.split("/")[2])) && key != '/locations/')
+                document.getElementById("overview").classList.add("active");
+            else
+                document.getElementById(pages[key]).classList.add("active");
+            break;
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 768px)")
+    hamburger(x)
+    x.addListener(hamburger)
 }
 
