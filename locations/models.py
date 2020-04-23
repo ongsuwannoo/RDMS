ce95fe59d8ce2d38fe526de3e9941350998907fa
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-04-24 01:06:41 +0700

    Add Location and model.py

--- locations/models.py
+++ locations/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-# class Camp(models.Model):
-#     head = models.OneToOneField(user, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=255)
-#     desc = models.TextField()
-#     start_date = models.DateField()
-#     end_date = models.DateField()
-#     logo = models.ImageField(upload_to='logo/')
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    logo = models.ImageField(upload_to='logo/')
--- locations/views.py
+++ locations/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import *
 from index.views import getPersonal
 
 # Create your views here.
 
-def locations(request):
-    context = getPersonal(request)
+def locations(request,id_location=""):
+    context = {}
+    context['id_location'] = id_location
+    if id_location:
+        location = Location.objects.get(pk=id_location)
+        context['locations'] = location
+        # context['active_camp'] = True
+    else:
+        location = Location.objects.all()
+        context['locations'] = location
     return render(request, 'locations.html', context)
+
+def create_location (request):
+    context = getPersonal(request)
+    if request.method == 'POST':
+        # user = request.user
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        # start_date = request.POST.get('start_date')
+        # end_date = request.POST.get('end_date')
+        logo = request.FILES.get('logo')
+        location = Location(
+            # head = user,
+            name = name,
+            desc = desc,
+            # start_date = start_date,
+            # end_date = end_date,
+            logo = logo
+        )
+        location.save()
+        return redirect('locations')
+    return render(request, 'create_location.html', context)
