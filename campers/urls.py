31f29265be8b53211665702f9e74bc02ae110700
Thanawat <thanawat.kemwat@gmail.com>
2020-04-23 23:31:14 +0700

    edit url campers

--- campers/urls.py
+++ campers/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.campers, name='campers'),
-    path('<int:number>/', views.camper_detail, name='camper_detail'),
+    path('<int:id_camp>/', views.camper_detail, name='camper_detail'),
     
 ]
