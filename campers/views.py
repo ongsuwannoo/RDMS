c5e55dc0359152252ed601c2b5dd9665af496ce3
Thanawat <thanawat.kemwat@gmail.com>
2020-04-27 06:48:11 +0700

    Delete models.py

--- campers/views.py
+++ campers/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from index.views import getPersonal
-
+from personal.models import Personal
+from .models import Camper
 # Create your views here.
 
 def campers(request, id_camp):
@@ -27,9 +28,25 @@ def create_camper(request, id_camp):
     context = {}
     context['id_camp'] = id_camp
     context['name'] = getPersonal(request)['name']
+    camper_detail = Camper.objects.all()
     if id_camp:
         context['active_camp'] = True
+        if request.method == 'POST':
+            camper = Personal.objects.create(
+                nick_name= request.POST.get('nick_name'),
+                blood_type = request.POST.get('blood_type'),
+                birthday = request.POST.get('birthday'),
+                religion = request.POST.get('religion'),
+                food_allergy = request.POST.get('food_allergy'),
+                congenital_disease = request.POST.get('congenital_disease'),
+                shirt_size = request.POST.get('shirt_size'),
+            )
+            print('successfully add to database')
+        else:
+            camper = Personal.objects.none()
+    
     return render(request, 'create_camper.html', context)
+
 
 # @csrf_exempt
 # def api(request):
