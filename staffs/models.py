fdfb3b8b46e27190841a62f38d70d3313cac8b94
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-04-27 15:38:33 +0700

    test

--- staffs/models.py
+++ staffs/models.py
@@ -7,6 +7,7 @@ class Staff(models.Model):
     camp = models.ForeignKey(to='camp.Camp', on_delete=models.CASCADE)
     personal = models.OneToOneField(Personal, on_delete=models.CASCADE)
     department = models.ForeignKey(to='camp.Department', on_delete=models.CASCADE, null=True, blank=True)
+    mc = models.ForeignKey(to='camp.MC', on_delete=models.CASCADE, null=True, blank=True)
     position = models.CharField(max_length=255)
     group = models.CharField(max_length=255)
     postscript = models.CharField(max_length=255, null=True)
