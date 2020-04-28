447c42093c0aa108ccd6d98b8ebc6e4ffe3ca124
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-04-28 20:42:46 +0700

    upload migrations

--- index/admin.py
+++ index/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-
-from index.models import user
+from django.contrib.auth.models import Permission
+from .models import *
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    def group(self, user):
+        groups = []
+        for group in user.groups.all():
+            groups.append(group.name)
+        return ' '.join(groups)
+    group.short_description = 'Groups'
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'group')
+
 admin.site.register(user, AuthorAdmin)
+admin.site.register(Personal)
+admin.site.register(Permission)
+
+admin.site.unregister(Group)
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
