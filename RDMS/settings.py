ec49796ba84c2af06f764f0a6572c7b26b92f078
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-05-02 03:17:47 +0700

    fixed 2

--- RDMS/settings.py
+++ RDMS/settings.py
@@ -148,7 +148,7 @@ AUTH_USER_MODEL = 'index.user'
 
 LOGIN_URL = '/login/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
